36de5eb1c696d9fd9901c5d3ff83ec65e890953c
Rupesh Nepal <rupeshrnepal@gmail.com>
2025-09-13 23:52:00 +0545

    feat(stats): visualize stats

--- handler/stats.go
+++ handler/stats.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 	"sort"
@@ -16,7 +17,6 @@ func errorPanic(err error) {
 }
 
 func GetFillCommits(email string, path string, commits map[int]int) map[int]int {
-
 	repo, err := git.PlainOpen(path)
 	errorPanic(err)
 	hRef, err := repo.Head()
@@ -39,8 +39,10 @@ func GetFillCommits(email string, path string, commits map[int]int) map[int]int 
 }
 
 func PrintCommitsStats(commit map[int]int) {
-        
-        
+	keys := sortMapIntoSlice(commit)
+	columns := buildColumns(keys, commit)
+	printCells(columns)
+
 }
 
 func sortMapIntoSlice(m map[int]int) (sortedKeys []int) {
@@ -103,6 +105,137 @@ func calculateOffset() (offset int) {
 	case time.Saturday:
 		offset = 1
 	}
-
 	return
+}
+
+func buildColumns(key []int, commits map[int]int) map[int][]int {
+	columns := make(map[int][]int)
+	var column []int
+
+	for _, k := range key {
+		week := int(k / 7)
+		dayinweek := k % 7
+		if dayinweek == 0 {
+			column = make([]int, 0)
+		}
+		column = append(column, commits[k])
+		if dayinweek == 0 {
+			columns[week] = column
+		}
+
+	}
+	return columns
+}
+
+
+func printCell(val int, today bool) {
+    escape := "\033[0;37;30m"
+    switch {
+    case val > 0 && val < 5:
+        escape = "\033[1;30;47m"
+    case val >= 5 && val < 10:
+        escape = "\033[1;30;43m"
+    case val >= 10:
+        escape = "\033[1;30;42m"
+    }
+
+    if today {
+        escape = "\033[1;37;45m"
+    }
+
+    if val == 0 {
+        fmt.Printf(escape + "  - " + "\033[0m")
+        return
+    }
+
+    str := "  %d "
+    switch {
+    case val >= 10:
+        str = " %d "
+    case val >= 100:
+        str = "%d "
+    }
+
+    fmt.Printf(escape+str+"\033[0m", val)
+}
+
+func printCells(cols map[int][]int) {
+	printMonths()
+	for j := 6; j >= 0; j-- {
+		for i := weeksInLastSixMonths() + 1; i >= 0; i-- {
+			if i == weeksInLastSixMonths()+1 {
+				printDayCol(j)
+			}
+			if col, ok := cols[i]; ok {
+				//special case today
+				if i == 0 && j == calculateOffset()-1 {
+					printCell(col[j], true)
+					continue
+				} else {
+					if len(col) > j {
+						printCell(col[j], false)
+						continue
+					}
+				}
+			}
+			printCell(0, false)
+		}
+		fmt.Printf("\n")
+	}
+}
+func getBeginningOfDay(t time.Time) time.Time {
+	year, month, day := t.Date()
+	startOfDay := time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+	return startOfDay
+}
+func printMonths() {
+	week := getBeginningOfDay(time.Now()).Add(-time.Duration(daysInLastSixMonths()*int(time.Hour)*24))
+	month := week.Month()
+	fmt.Printf("         ")
+	for {
+		if week.Month() != month {
+			fmt.Printf("%s ", week.Month().String()[:3])
+			month = week.Month()
+		} else {
+			fmt.Printf("    ")
+		}
+
+		week = week.Add(7 * time.Hour * 24)
+		if week.After(time.Now()) {
+			break
+		}
+	}
+	fmt.Printf("\n")
+}
+
+// printDayCol given the day number (0 is Sunday) prints the day name,
+// alternating the rows (prints just 2,4,6)
+func printDayCol(day int) {
+	out := "     "
+	switch day {
+	case 1:
+		out = " Mon "
+	case 3:
+		out = " Wed "
+	case 5:
+		out = " Fri "
+	}
+
+	fmt.Printf(out)
+}
+func ProcessRepositories(email string) map[int]int {
+    filePath := GetDotfilePath()
+    repos := parseFileLinesToString(filePath)
+    daysInMap := daysInLastSixMonths()
+
+    commits := make(map[int]int, daysInMap)
+    for i := daysInMap; i > 0; i-- {
+        commits[i] = 0
+    }
+
+    for _, path := range repos {
+        commits = GetFillCommits(email, path, commits)
+    }
+
+    return commits
 }
--- main.go
+++ main.go
@@ -10,34 +10,30 @@ func scan(path string) {
 	fmt.Println("... FOUND FOLDER")
 	repo := handler.RecursivelyScanFolder(path)
 	fmt.Printf("%s\n", repo)
-        filePath:=handler.GetDotfilePath()
-        fmt.Printf("%s\n",filePath)
-        handler.AddNewSliceToFile(filePath,repo)
-        fmt.Printf("... Added to %s file\n",filePath)        
+	filePath := handler.GetDotfilePath()
+	fmt.Printf("%s\n", filePath)
+	handler.AddNewSliceToFile(filePath, repo)
+	fmt.Printf("... Added to %s file\n", filePath)
 }
 
 func stats(email string) {
-	println("stats")
-	println(email)
-
+	commits:= handler.ProcessRepositories(email)
+	handler.PrintCommitsStats(commits)
 }
 
-
-
 func main() {
-          
+
 	var folder string
 	var email string
 	flag.StringVar(&folder, "add", "", "Add a folder to the scan list of your git repository")
 	flag.StringVar(&email, "stats", "your@email.com", "Get the stats of the email")
 	flag.Parse()
-
 	if folder != "" {
 		scan(folder)
 	}
-        
-        if email != "" {
-                stats(email)
-        }
+
+	if email != "" {
+		stats(email)
+	}
 
 }
